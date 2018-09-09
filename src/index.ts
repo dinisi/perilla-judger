4fe07705f1489c27b4d035abf1a32ab88250da3c
ZhangZisu <admin@zhangzisu.cn>
2018-09-09 15:39:25 +0800

    remove remote judge (add it next time, without fucking chrome)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { promisifyAll } from "bluebird";
 import * as redis from "redis";
-import { launchChrome } from "./chromeHelper";
 import { initialize } from "./http";
 import { IJudgerConfig, IProblemModel, ISolutionModel } from "./interfaces";
 import { getProblem } from "./problem";
@@ -25,7 +24,6 @@ const choose = async (config: IJudgerConfig, solution: ISolutionModel, problem: 
 
 const startJudge = async (config: IJudgerConfig) => {
     await initialize(config);
-    await launchChrome();
     const judgeLoop = async () => {
         const solutionID = (await instance.rpopAsync("judgeTask"));
         if (solutionID) {
