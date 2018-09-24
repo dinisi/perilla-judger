f11bbd440e31763067e398f6b971cedd6e1d9011
ZhangZisu <admin@zhangzisu.cn>
2018-09-24 16:56:59 +0800

    Upload

--- test/src/index.ts
+++ test/src/index.ts
@@ -2,23 +2,9 @@ import { LYDSYRobot } from "./lydsy";
 import { UOJRobot } from "./uoj";
 
 (async () => {
-    const robot = new UOJRobot("zhangzisu_develop", "78d58440fec80641ab0a63e1d4a27337");
+    const robot = new UOJRobot("zhangzisu_develop", "123456");
     await robot.initalize();
-    const runID = await robot.submit("1000",
-    `
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int a,b;
-    cin >> a >> b;
-    cout << a+b << endl;
-    return 0;
-}
-    `, "cpp");
     // tslint:disable-next-line:no-console
-    console.log(runID);
+    console.log(await robot.fetch("287100"));
     // const result = await robot.fetch()
 })();
