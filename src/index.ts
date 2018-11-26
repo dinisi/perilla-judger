9a2ab93c5ef1ad0c69ca74414f3519fdd3960716
ZhangZisu <admin@zhangzisu.cn>
2018-11-26 16:57:08 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { readdir, readdirSync, readFileSync, statSync } from "fs-extra";
-import { join, resolve } from "path";
+import { readdirSync, readFileSync, statSync } from "fs-extra";
+import { random } from "lodash";
+import { join } from "path";
 import { getFile } from "./file";
 import { get, initialize, post } from "./http";
 import { ITask, JudgeFunction, SolutionResult } from "./interfaces";
 
 const pluginDir = join(__dirname, "..", "plugins");
-const channels = new Set<string>();
+const channelSet = new Set<string>();
 
 const isPlugin = (dir: string) => {
     const stat = statSync(dir);
@@ -17,14 +18,21 @@ for (const file of readdirSync(pluginDir)) {
     // tslint:disable-next-line:no-console
     console.log("[MAIN] found: %s", file);
     if (!isPlugin(join(pluginDir, file))) { continue; }
-    channels.add(file);
+    channelSet.add(file);
+}
+
+if (!channelSet) {
+    // tslint:disable-next-line:no-console
+    console.log("No plugin found");
+    process.exit(0);
 }
 
 const config = JSON.parse(readFileSync("config.json").toString());
 
 initialize(config.server, config.username, config.password).then(() => {
-    const channel = [...channels];
+    const channels = [...channelSet];
     const process = () => {
+        const channel = channels[random(0, channels.length - 1)];
         get("/api/judger/pop", { channel })
             .then((task: ITask) => {
                 const judge = require(join(pluginDir, task.channel)) as JudgeFunction;
@@ -56,7 +64,7 @@ initialize(config.server, config.username, config.password).then(() => {
                     });
                 });
             })
-            .catch((err) => {
+            .catch(() => {
                 // Empty queue, sleep 1 sec
                 setTimeout(process, 1000);
             });
